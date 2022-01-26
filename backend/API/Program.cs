da4c8a5d7b5fffbaadfa7edeb2d5499e8ad78b02
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-01-26 20:20:10 +0300

    Yapılandırma ayarları değiştirildi

--- backend/API/Program.cs
+++ backend/API/Program.cs
@@ -1,16 +1,36 @@
 using Data.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Services.Extensions;
 using Shared.Configurations;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
-builder.Services.Configure<CustomTokenOption>(builder.Configuration.GetSection("TokenOption"));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.DataLayerServiceCollection(builder.Configuration);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(x =>
+    {
+        x.RequireHttpsMetadata = false;
+        x.SaveToken = true;
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration.GetSection("JwtKey").ToString())),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 builder.Services.ServiceLayerServiceCollection();
 var app = builder.Build();
 
