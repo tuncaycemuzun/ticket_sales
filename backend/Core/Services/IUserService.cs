de1db4eaefed598b0c561aa0cba750d6d5ffa7be
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-01-26 20:18:42 +0300

    Core katmanında IUserService oluşturuldu

--- backend/Core/Services/IUserService.cs
+++ backend/Core/Services/IUserService.cs
@@ -1,4 +1,5 @@
 ﻿using Core.Dtos;
+using Shared.Utilities.Results.Abstract;
 using Shared.Utilities.Results.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,7 @@ namespace Core.Services
 {
     public interface IUserService
     {
-        Task<DataResult<UserDto>> AddAsync(CreateUserDto createUserDto);
+        Task<IDataResult<UserDto>> AddAsync(CreateUserDto createUserDto);
+        Task<IDataResult<TokenDto>> CreateToken(LoginDto loginDto);
     }
 }
