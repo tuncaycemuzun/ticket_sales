13b1a80ef53cc85bd8d47c9e13521a2612a1702f
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-01-26 15:27:16 +0300

    Created ServiceCollection

---
+++ backend/Data/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,30 @@
+﻿using Core.Abstract;
+using Data.Repositories;
+using Data.Settings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddMongoDbSettings(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddSingleton<IUserDal, UserDal>();
+
+            return services.Configure<MongoDbSettings>(options =>
+            {
+                options.ConnectionString = configuration
+                    .GetSection(nameof(MongoDbSettings) + ":" + MongoDbSettings.ConnectionStringValue).Value;
+                options.Database = configuration
+                    .GetSection(nameof(MongoDbSettings) + ":" + MongoDbSettings.DatabaseValue).Value;
+            });
+        }
+    }
+}
