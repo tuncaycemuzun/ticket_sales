a252731e649d8f5b08b13159f3207605f9ee20c1
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-01-26 18:05:27 +0300

    created IUserService

---
+++ backend/Core/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Dtos;
+using Shared.Utilities.Results.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface IUserService
+    {
+        Task<DataResult<UserDto>> AddAsync(CreateUserDto createUserDto);
+    }
+}
