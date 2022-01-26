159c303cf37584f395505eab94bb405e7d2f9686
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-01-26 18:03:40 +0300

    Created User Controller

---
+++ backend/API/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Core.Abstract;
+using Core.Concrete;
+using Core.Dtos;
+using Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using Shared.Utilities.Results.ComplexTypes;
+using Shared.Utilities.Results.Concrete;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Add(CreateUserDto createUserDto)
+        {
+            var result = await _userService.AddAsync(createUserDto);
+            return Json(result);
+        }
+    }
+}
