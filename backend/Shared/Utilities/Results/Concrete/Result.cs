136e461303c9d1b8f3c61c29e53202326b38719f
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-01-26 14:08:03 +0300

    The Shared layer has been created. Result structure has been integrated.

---
+++ backend/Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,32 @@
+﻿using Shared.Utilities.Results.Abstract;
+using Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Utilities.Results.Concrete
+{
+    internal class Result : IResult
+    {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+        public Result(ResultStatus resultStatus, string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public Result(ResultStatus resultStatus, string message, Exception e)
+        {
+            Exception = e;
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+    }
+}
