8b277de35e67bd9dd120d6e5a0aff1d0d038ac6b
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-02-18 12:53:57 +0300

    added new dtos mapping

--- backend/Services/DtoMapper.cs
+++ backend/Services/DtoMapper.cs
@@ -12,6 +12,11 @@ namespace Services
             CreateMap<CreateUserDto, User>().ReverseMap();
             CreateMap<LoginDto, User>().ReverseMap();
             CreateMap<TokenDto, UserToken>().ReverseMap();
+            CreateMap<TicketDto, Ticket>().ReverseMap();
+            CreateMap<TicketAddDto, Ticket>().ReverseMap();
+            CreateMap<TicketUpdateDto, Ticket>().ReverseMap();
+            CreateMap<TicketTypeAddDto, TicketType>().ReverseMap();
+            CreateMap<TicketTypeUpdateDto, TicketType>().ReverseMap();
         }
     }
 }
