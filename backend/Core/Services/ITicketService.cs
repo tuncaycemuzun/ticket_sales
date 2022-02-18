fa963eeaec582cca64fa7d9e34513bf3e1abf54a
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-02-18 12:53:25 +0300

    create ticket service and ticket type service interface

---
+++ backend/Core/Services/ITicketService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Concrete;
+using Core.Dtos;
+using MongoDB.Bson;
+using Shared.Utilities.Results.Abstract;
+
+namespace Core.Services
+{
+    public interface ITicketService
+    {
+        Task<IResult> AddAsync(TicketAddDto ticketAddDto);
+        Task<IResult> DeleteAsync(string id);
+        Task<IDataResult<TicketUpdateDto>> UpdateAsync(string id,TicketUpdateDto ticketUpdateDto);
+        Task<IDataResult<IList<TicketDto>>> GetAsync();
+    }
+}
---
+++ backend/Core/Services/ITicketTypeService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Concrete;
+using Core.Dtos;
+using Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface ITicketTypeService
+    {
+        Task<IResult> AddAsync(TicketTypeAddDto ticketTypeAddDto);
+        Task<IResult> DeleteAsync(string id);
+        Task<IDataResult<TicketTypeUpdateDto>> UpdateAsync(string id,TicketTypeUpdateDto ticketTypeUpdate);
+        Task<IDataResult<IList<TicketType>>> GetAsync();
+    }
+}
