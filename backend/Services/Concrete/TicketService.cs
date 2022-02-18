ef838720c788b6f56c6ea13ea5fdcb40bb16a8b0
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-02-18 12:53:40 +0300

    create ticket service and ticket type service

---
+++ backend/Services/Concrete/TicketService.cs
@@ -0,0 +1,56 @@
+﻿using Core.Abstract;
+using Core.Concrete;
+using Core.Dtos;
+using Core.Services;
+using MongoDB.Bson;
+using Shared.Utilities.Results.Abstract;
+using Shared.Utilities.Results.ComplexTypes;
+using Shared.Utilities.Results.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Concrete
+{
+    public class TicketService : ITicketService
+    {
+        private readonly ITicketRepository _repo;
+        public TicketService(ITicketRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IResult> AddAsync(TicketAddDto ticketAddDto)
+        {
+            var ticket = ObjectMapper.Mapper.Map<Ticket>(ticketAddDto);
+            var result = await _repo.AddAsync(ticket);
+            if (result == null) return new Result(ResultCode.BadRequest);
+            return new Result(ResultCode.Success);
+        }
+
+        public async Task<IResult> DeleteAsync(string id)
+        {
+            var entity = await _repo.GetAsync(x => x.Id.ToString() == id);
+            if (entity == null) return new Result(ResultCode.NotFound,Messages.Ticket.NotFound());
+
+            await _repo.DeleteAsync(entity);
+            return new Result(ResultCode.Success, Messages.Ticket.Success());
+        }
+
+        public Task<IDataResult<IList<TicketDto>>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IDataResult<TicketUpdateDto>> UpdateAsync(string id,TicketUpdateDto ticketUpdateDto)
+        {
+            var entity = await _repo.GetAsync(x => x.Id.ToString() == id);
+            if(entity==null) return new DataResult<TicketUpdateDto>(ResultCode.NotFound, Messages.Ticket.NotFound());
+            var ticket = ObjectMapper.Mapper.Map<TicketUpdateDto>(entity);
+            await _repo.UpdateAsync(entity.Id.ToString(), entity);
+            return new DataResult<TicketUpdateDto>(ResultCode.Success, Messages.Ticket.Success(), ticket);
+        }
+    }
+}
---
+++ backend/Services/Concrete/TicketTypeService.cs
@@ -0,0 +1,47 @@
+﻿using Core.Abstract;
+using Core.Concrete;
+using Core.Dtos;
+using Core.Services;
+using MongoDB.Bson;
+using Shared.Utilities.Results.Abstract;
+using Shared.Utilities.Results.ComplexTypes;
+using Shared.Utilities.Results.Concrete;
+
+namespace Services.Concrete
+{
+    public class TicketTypeService : ITicketTypeService
+    {
+        private readonly ITicketTypeRepository _repo;
+        public TicketTypeService(ITicketTypeRepository repo)
+        {
+            _repo = repo;
+        }
+        public async Task<IResult> AddAsync(TicketTypeAddDto ticketTypeAddDto)
+        {
+            var ticket = ObjectMapper.Mapper.Map<TicketType>(ticketTypeAddDto);
+            await _repo.AddAsync(ticket);
+            return new Result(ResultCode.Success);
+        }
+
+        public async Task<IResult> DeleteAsync(string id)
+        {
+            var entity = await _repo.GetByIdAsync(id);
+            if (entity == null) return new Result(ResultCode.NotFound);
+            await _repo.DeleteAsync(entity);
+            return new Result(ResultCode.Success);
+        }
+
+        public Task<IDataResult<IList<TicketType>>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IDataResult<TicketTypeUpdateDto>> UpdateAsync(string id,TicketTypeUpdateDto ticketTypeUpdate)
+        {
+            var entity = ObjectMapper.Mapper.Map<TicketType>(ticketTypeUpdate);
+            await _repo.UpdateAsync(id, entity);
+            return new DataResult<TicketTypeUpdateDto>(ResultCode.Success, Messages.Ticket.Success(), ticketTypeUpdate);
+
+        }
+    }
+}
