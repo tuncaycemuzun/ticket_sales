01600df15244bd61bc4ce68a49cb912b8f8c4eeb
tuncaycemuzun <tuncaycemuzun@outlook.com>
2022-02-18 12:53:47 +0300

    created controllers

---
+++ backend/API/Controllers/TicketController.cs
@@ -0,0 +1,53 @@
+﻿using Core.Concrete;
+using Core.Dtos;
+using Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Utilities.Results.ComplexTypes;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketController : Controller
+    {
+        private readonly ITicketService _ticketService;
+        public TicketController(ITicketService ticketService)
+        {
+            _ticketService = ticketService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Add(TicketAddDto ticketAddDto)
+        {
+            var result = await _ticketService.AddAsync(ticketAddDto);
+            if(result.ResultCode == ResultCode.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var result = await _ticketService.DeleteAsync(id);
+            if (result.ResultCode == ResultCode.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+
+
+        [HttpPut]
+        [Authorize]
+        public async Task<IActionResult> Update(string id,TicketUpdateDto ticketUpdateDto)
+        {
+            var result = await _ticketService.UpdateAsync(id,ticketUpdateDto);
+            if (result.ResultCode == ResultCode.Success)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
---
+++ backend/API/Controllers/TicketTypeController.cs
@@ -0,0 +1,44 @@
+﻿using Core.Dtos;
+using Core.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TicketTypeController : Controller
+    {
+        private readonly ITicketTypeService _ticketTypeService;
+        public TicketTypeController(ITicketTypeService ticketTypeService)
+        {
+            _ticketTypeService = ticketTypeService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Add(TicketTypeAddDto ticketTypeAddDto)
+        {
+            var result = await _ticketTypeService.AddAsync(ticketTypeAddDto);
+            return Ok(result);
+        }
+
+
+        [HttpDelete]
+        [Authorize]
+        public async Task<IActionResult> Delete(TicketTypeDeleteDto ticketTypeDeleteDto)
+        {
+            var result = await _ticketTypeService.DeleteAsync(ticketTypeDeleteDto.Id);
+            return Ok(result);
+        }
+
+
+        [HttpPut]
+        [Authorize]
+        public async Task<IActionResult> Update(string id,TicketTypeUpdateDto ticketTypeUpdateDto)
+        {
+            var result = await _ticketTypeService.UpdateAsync(id,ticketTypeUpdateDto);
+            return Ok(result);
+        }
+    }
+}
